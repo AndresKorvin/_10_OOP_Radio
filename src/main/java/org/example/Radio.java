7f80e08e6154d12d130ebdbfc28089fe6275be71
AndresKorvin <andreskorvin@gmail.com>
2023-01-28 18:50:12 +0300

    ...

--- src/main/java/org/example/Radio.java
+++ src/main/java/org/example/Radio.java
@@ -1,8 +1,16 @@
 package org.example;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
 public class Radio {
-    private int currentStation = 0;
-    private int currentVolume = 0;
+    private int currentStation;
+    private int currentVolume;
     private  int amountOfStations = 9;
 
     public Radio (int amountOfStations) {
@@ -11,28 +19,15 @@ public class Radio {
         }
     }
 
-    public Radio() {
-        System.out.println("Установлено 10 станций");
-    }
-
-    public int getAmountOfStations() {
-        return this.amountOfStations;
-    }
-
     public void setCurrentStation(int stationButton) {
         if (stationButton >= 0 && stationButton <= this.amountOfStations) {
             this.currentStation = stationButton;
         }
     }
 
-    public int getCurrentStation() {
-        return this.currentStation;
-    }
-
     public void nextStation() {
         if (this.currentStation < this.amountOfStations) {
             ++this.currentStation;
-
         } else {
             this.currentStation = 0;
         }
@@ -41,7 +36,6 @@ public class Radio {
     public void previousStation() {
         if (this.currentStation > 0) {
             --this.currentStation;
-
         } else {
             this.currentStation = this.amountOfStations;
         }
@@ -51,24 +45,17 @@ public class Radio {
         if (volumeLevel >= 0 && volumeLevel <= 100) {
             this.currentVolume = volumeLevel;
         }
-
-    }
-
-    public int getCurrentVolume() {
-        return this.currentVolume;
     }
 
     public void increaseVolume() {
         if (this.currentVolume < 100) {
             this.currentVolume += 5;
         }
-
     }
 
     public void reduceVolume() {
         if (this.currentVolume > 0) {
             this.currentVolume -= 5;
         }
-
     }
 }
